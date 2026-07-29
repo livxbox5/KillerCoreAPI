f7e5b203a5a0c6d032c4ea39bdee7ece9a062828
livxbox5 <76809274+livxbox5@users.noreply.github.com>
2026-07-29 15:51:52 +0300

    Update LanguageManager.java

--- src/main/java/org/killeryt/killerCoreAPI/utils/LanguageManager.java
+++ src/main/java/org/killeryt/killerCoreAPI/utils/LanguageManager.java
@@ -1,39 +1,59 @@
 package org.killeryt.killerCoreAPI.utils;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- * Менеджер языковых файлов.
- */
 public class LanguageManager {
-
     private final JavaPlugin plugin;
-    private FileConfiguration langConfig;
-    private final String defaultLang;
+    private final String language;
+    private FileConfiguration config;
+    private final Map<String, String> cache = new HashMap<>();
 
-    public LanguageManager(JavaPlugin plugin, String defaultLang) {
+    public LanguageManager(JavaPlugin plugin, String language) {
         this.plugin = plugin;
-        this.defaultLang = defaultLang;
-        reloadLanguage();
+        this.language = language;
+        reload();
     }
 
-    public void reloadLanguage() {
-        String lang = plugin.getConfig().getString("lang", defaultLang);
-        File langFile = new File(plugin.getDataFolder(), "lang/" + lang + ".yml");
+    public void reload() {
+        File langFolder = new File(plugin.getDataFolder(), "lang");
+        if (!langFolder.exists()) langFolder.mkdirs();
+
+        File langFile = new File(langFolder, language + ".yml");
         if (!langFile.exists()) {
-            plugin.saveResource("lang/" + lang + ".yml", false);
+            plugin.saveResource("lang/" + language + ".yml", false);
         }
-        langConfig = YamlConfiguration.loadConfiguration(langFile);
+        config = YamlConfiguration.loadConfiguration(langFile);
+        cache.clear();
     }
 
-    public String get(String path) {
-        return ColorUtils.color(langConfig.getString(path, "§cСообщение не найдено: " + path));
+    public String get(String key) {
+        return get(key, new Object[0]);
     }
 
-    public String get(String path, String defaultValue) {
-        return ColorUtils.color(langConfig.getString(path, defaultValue));
+    public String get(String key, Object... args) {
+        if (cache.containsKey(key)) {
+            return String.format(cache.get(key), args);
+        }
+        String message = config.getString(key);
+        if (message == null) {
+            message = "&cMissing language key: " + key;
+        }
+        cache.put(key, message);
+        return String.format(message, args);
+    }
+
+    public void send(CommandSender sender, String key, Object... args) {
+        String message = get(key, args);
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
     }
 }
